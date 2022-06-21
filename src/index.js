8c358eae110526f1a64da274b555d05ceb568906
subrat50 <subrats400@gmail.com>
2022-06-21 13:42:27 +0530

    subrat

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://subrat_400:4iQC1DP0ZqKInrD3@cluster0.h3xeivd.mongodb.net/chentan40-", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
---
+++ src/models/blogModel.js
@@ -0,0 +1,39 @@
+const mongoose=require('mongoose')
+const ObjectId=mongoose.Schema.Types.ObjectId
+
+const blogSchema= new mongoose.Schema({
+ title: {
+    type:String,
+    required:true
+},
+ body: {
+    type:String,
+    required:true
+    },
+ authorId: {
+     type:ObjectId,
+    required:true,
+    ref:'Author'
+ },
+  tags: [String],
+  category: {
+    type:[String],
+    required:true
+    },
+   subcategory: [String], 
+   deletedAt: {
+    type:Date
+    },
+    isDeleted: {
+        type:Boolean ,
+        default: false
+    }, 
+    publishedAt: {
+        type:Date
+    }, 
+    isPublished: {
+        type:Boolean, 
+        default: false,
+    }
+},{timestamps:true})
+module.exports=mongoose.model('Blog',blogSchema)
